9a61320a5b7e877f34c58ec94e867187c668d20c
Hiu Ming Tse <tse00022@algonquinlive.com>
2024-10-09 16:44:04 -0400

    First commit

---
+++ bin/mad9135_exercise_3.dart
@@ -0,0 +1,5 @@
+import 'package:mad9135_exercise_3/mad9135_exercise_3.dart' as mad9135_exercise_3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mad9135_exercise_3.calculate()}!');
+}
---
+++ test/mad9135_exercise_3_test.dart
@@ -0,0 +1,8 @@
+import 'package:mad9135_exercise_3/mad9135_exercise_3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
